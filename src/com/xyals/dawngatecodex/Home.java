885c44750d928909035a06e0fab850be67b44b2b
xialouis <louis.xlxu@gmail.com>
2013-09-15 21:34:47 -0400

    added home activity button method calls and Home.java switch statement for creating new activities

--- src/com/xyals/dawngatecodex/Home.java
+++ src/com/xyals/dawngatecodex/Home.java
@@ -3,6 +3,7 @@ package com.xyals.dawngatecodex;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.view.View;
 
 public class Home extends Activity {
 
@@ -18,6 +19,27 @@ public class Home extends Activity {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.home, menu);
         return true;
+    }
+    
+    // Handle button clicks on home screen
+    public void homeButtonClicked(View view) {
+    	// Open the appropriate screen depending on button clicked
+    	switch(view.getId()) {
+    	case(R.id.shapersButton):
+    		break;
+    	
+    	case(R.id.itemsButton):
+    		break;
+    	
+    	case(R.id.minimapButton):
+    		break;
+    	
+    	case(R.id.buildsButton):
+    		break;
+    	
+    	default:
+    		break;
+    	}
     }
     
 }
